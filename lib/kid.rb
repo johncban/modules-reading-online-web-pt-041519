64b979e231e82c6efe62fbda46567d040d0197f1
Juan Carlo Banayo <johncban@gmail.com>
2019-05-27 17:52:48 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,9 @@ require_relative './class_methods_module.rb'
 
 class Kid
 
-  extend MetaDancing
+  # extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   include Dance
 
