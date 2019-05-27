c9ce0b098fcc4e768434d269afc1fe3ad19902d1
Juan Carlo Banayo <johncban@gmail.com>
2019-05-27 17:57:17 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -16,3 +16,16 @@ class Dancer
     @name = name
   end
 end
+
+
+angelina = Dancer.new
+angelina.twirl
+// returns "I'm twirling!"
+angelina.jump
+// returns "Look how high I'm jumping!"
+
+buster = Kid.new
+buster.jump
+// returns "Look how high I'm jumping!"
+buster.take_a_bow
+// returns "Thank you, thank you. It was a pleasure to dance for you all."
